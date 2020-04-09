d16a311b25d17cd81030a017d676af6247e98ce3
songjianyang <554723288@qq.com>
2020-04-09 21:07:58 +0800

    Compiler

---
+++ lexer.py
@@ -0,0 +1,159 @@
+NUMBER, STRING, ID, WORD = 'NUMBER', 'STRING', 'ID', 'WORD'
+ADD, SUB, MUL, DIV, ASSIGN = 'ADD', 'SUB', 'MUL', 'DIV', 'ASSIGN'
+CMPGT, CMPLT, EQUAL = 'CMPGT', 'CMPLT', 'EQUAL'
+LBRACKET, RBRACKET, LPAREN, RPAREN = 'LBRACKET', 'RBRACKET', 'LPAREN', 'RPAREN'
+COMMA, QUOT, PERCENT = 'COMMA', 'QUOT', 'PERCENT'
+LINEEND, SPACE, EOF = ';', ' ', 'EOF'
+
+
+class Token():
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return 'Token({type}, {value})'.format(
+            type=self.type,
+            value=repr(self.value))
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Lexer():
+    def __init__(self, text):
+        self.text = self.readfile(text)
+        self.pos = 0
+        self.isEOF = False
+
+    def get_number(self):
+        result = ''
+        while self.pos <= len(self.text) - 1 and self.text[self.pos].isdigit():
+            result += self.text[self.pos]
+            self.pos += 1
+        return int(result)
+
+    def get_word(self):
+        result = ''
+        while self.pos <= len(self.text) - 1 and self.text[self.pos].isalpha():
+            result += self.text[self.pos]
+            self.pos += 1
+        return result
+
+    def get_str(self):
+        result = '"'
+        self.pos += 1
+        while self.pos <= len(self.text) - 1 and self.text[self.pos] != '"':
+            result += self.text[self.pos]
+            self.pos += 1
+        self.pos += 1
+        result += '"'
+        return result
+
+    def skip_space(self):
+        while self.pos <= len(self.text) - 1 and self.text[self.pos] == SPACE:
+            self.pos += 1
+        return self.pos
+
+    def get_next_token(self):
+        text = self.text
+        pos = self.pos
+
+        if self.pos > len(text) - 1:
+            self.isEOF = True
+            return Token(EOF, None)
+        if text[pos] == SPACE:
+            self.skip_space()
+            return self.get_next_token()
+        if text[pos] == LINEEND:
+            self.pos += 1
+            return self.get_next_token()
+
+        if text[pos].isalpha():
+            content = self.get_word()
+            if content in ('if', 'while', 'else', 'def', 'print', 'return'):
+                token = Token(ID, content)
+            else:
+                token = Token(WORD, content)
+            return token
+        elif text[pos].isdigit():
+            token = Token(NUMBER, self.get_number())
+            return token
+        elif text[pos] == '"':
+            token = Token(STRING, self.get_str())
+            return token
+        elif text[pos] == '+':
+            token = Token(ADD, text[pos])
+            self.pos += 1
+            return token
+        elif text[pos] == '-':
+            token = Token(SUB, text[pos])
+            self.pos += 1
+            return token
+        elif text[pos] == '*':
+            token = Token(MUL, text[pos])
+            self.pos += 1
+            return token
+        elif text[pos] == '/':
+            token = Token(DIV, text[pos])
+            self.pos += 1
+            return token
+        elif text[pos] == '{':
+            token = Token(LBRACKET, text[pos])
+            self.pos += 1
+            return token
+        elif text[pos] == '}':
+            token = Token(RBRACKET, text[pos])
+            self.pos += 1
+            return token
+        elif text[pos] == '>':
+            token = Token(CMPGT, text[pos])
+            self.pos += 1
+            return token
+        elif text[pos] == '<':
+            token = Token(CMPLT, text[pos])
+            self.pos += 1
+            return token
+        elif text[pos] == ',':
+            token = Token(COMMA, text[pos])
+            self.pos += 1
+            return token
+        elif text[pos] == '(':
+            token = Token(LPAREN, text[pos])
+            self.pos += 1
+            return token
+        elif text[pos] == ')':
+            token = Token(RPAREN, text[pos])
+            self.pos += 1
+            return token
+        elif text[pos] == '%':
+            token = Token(PERCENT, text[pos])
+            self.pos += 1
+            return token
+        elif text[pos] == '=':
+            if text[pos + 1] == '=':
+                token = Token(EQUAL, '==')
+                self.pos += 2
+            else:
+                token = Token(ASSIGN, text[pos])
+                self.pos += 1
+            return token
+
+        self.pos += 1
+
+    def lexer(self):
+        result = []
+        while self.isEOF == False:
+            result.append(self.get_next_token())
+        return result
+
+    def readfile(self, filename):
+        text = ''
+        for line in file(filename):
+            line.strip()
+            line = line.replace('\n', ' ')
+            text += line
+        return text
+
+
+
---
+++ vm.py
@@ -0,0 +1,190 @@
+import re
+import sys
+from parser import *
+
+eip = 0
+eflag = 0
+stack = []
+var_table = {}
+param_num = []
+ret_addr = []
+
+code = Parser(sys.argv[1]).program()
+
+def do_push(arg):
+    global eip
+    global ret_addr
+    if arg in var_table:
+        index = var_table[arg]
+        stack.append(stack[index])
+    elif 'eip' in str(arg):
+        ret_addr.append(int(eip + 1))
+    else:
+        stack.append(arg)
+
+
+def do_pop(arg):
+    if arg:
+        index = var_table[arg]
+        stack[index] = stack.pop()
+    else:
+        stack.pop()
+
+
+def do_var(arg):
+    global var_table
+    var_table.update({arg: len(stack)})
+    stack.append(0)
+
+
+def do_add(arg):
+    num1 = stack.pop()
+    num2 = stack.pop()
+    stack.append(int(num2) + int(num1))
+
+
+def do_sub(arg):
+    num1 = stack.pop()
+    num2 = stack.pop()
+    stack.append(int(num2) - int(num1))
+
+
+def do_div(arg):
+    num1 = stack.pop()
+    num2 = stack.pop()
+    stack.append(int(num2) / int(num1))
+
+
+def do_mul(arg):
+    num1 = stack.pop()
+    num2 = stack.pop()
+    stack.append(int(num2) * int(num1))
+
+
+def do_cmpgt(arg):
+    global eflag
+    num1 = stack.pop()
+    num2 = stack.pop()
+    if num2 > num1:
+        eflag = 1
+    else:
+        eflag = 0
+
+
+def do_cmplt(arg):
+    global eflag
+    num1 = stack.pop()
+    num2 = stack.pop()
+    if num2 < num1:
+        eflag = 0
+    else:
+        eflag = 1
+
+
+def do_cmp(arg):
+    global eflag
+    num1 = stack.pop()
+    num2 = stack.pop()
+    if num2 == num1:
+        eflag = 1
+    else:
+        eflag = 0
+
+
+def do_jge(arg):
+    global eip
+    if eflag == 1:
+        eip = code.index([arg])
+
+
+def do_jle(arg):
+    global eip
+    if eflag == 0:
+        eip = code.index([arg])
+
+
+def do_jne(arg):
+    global eip
+    if eflag == 0:
+        eip = code.index([arg])
+
+
+def do_jmp(arg):
+    global eip
+    eip = code.index([arg])
+
+
+def do_call(arg):
+    global var_table
+    global eip
+    eip = code.index([arg])
+
+
+def do_ret(arg):
+    global var_table
+    global eip
+    global ret_addr
+    global param_num
+
+    stack.pop()
+    eip = int(ret_addr.pop())
+
+    pn = param_num.pop()
+    for i in xrange(pn):
+        stack.pop()
+
+
+def do_arg(arg):
+    global var_table
+    global param_num
+    ebp = var_table['ebp']
+    if ',' in arg:
+        n = 1
+        params = arg.split(',')
+        params.reverse()
+        param_num.append(len(params))
+        for i in params:
+            var_table.update({i: ebp - n})
+            n += 1
+    else:
+        n = 0 if arg == '' else 1
+        param_num.append(n)
+        var_table.update({arg: ebp - n})
+
+
+def do_print(arg):
+    global var_table
+
+    if '|' in arg:
+        fmt, var = arg.split('|')
+        fmt_p = re.findall(r'%[sd]', fmt)
+        var_p = var.split(',')
+
+        for i in xrange(0, len(var_p)):
+            v = stack[var_table[var_p[i]]]
+            v = str(fmt_p[i] % v)
+            fmt = fmt.replace(fmt_p[i], v, 1).replace('"', '')
+        print fmt.replace('\\n', '\n'),
+    else:
+        print eval(str(arg)),
+
+
+def do_exit(arg):
+    sys.exit()
+
+
+def run():
+    global eip
+    opcode, operands = 0, 0
+    while True:
+        code_line = code[eip]
+        opcode = code_line[0]
+        eip += 1
+        if len(code_line) > 1:
+            operands = code_line[1]
+        if ':' not in opcode:
+            action = eval('do_' + opcode)
+            action(operands)
+
+
+run()
